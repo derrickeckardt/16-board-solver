755bf6d179f6f22bd3d9e6e40b7cf96692e0f8fe
derrick eckardt <derrick.eckardt@gmail.com>
2018-09-23 01:33:08 +0000

    Part 1 partly working

--- part1/solver16.py
+++ part1/solver16.py
@@ -35,16 +35,27 @@ def reverse_move(state):
 # check if we've reached the goal
 def is_goal(state):
     return sorted(state) == list(state)
-    
+
+# Heuristic Function 1: Number of misplaced tiles
+def heurisitic_one(state, goal_state):
+    heur_one = sum(([1 if state[i-1] != goal_state[i-1] else 0 for i in state ]))
+    return heur_one
+
 # The solver! - using BFS right now
 def solve(initial_board):
-    fringe = [ (initial_board, "") ]
-    while len(fringe) > 0:
-        (state, route_so_far) = fringe.pop()
+    fringe = PriorityQueue()
+    fringe.put((heurisitic_one(initial_board,goal_state),[(initial_board),"",0]))
+    i = 0
+    while not fringe.empty() > 0:
+        (heuristic_value, fringeitem) = fringe.get()
+        [state, route_so_far,moves_so_far] = fringeitem
+#        print heuristic_value, moves_so_far, i
         for (succ, move) in successors( state ):
+#            print heurisitic_one(succ, goal_state)
             if is_goal(succ):
                 return( route_so_far + " " + move )
-            fringe.insert(0, (succ, route_so_far + " " + move ) )
+            fringe.put((heurisitic_one(succ,goal_state)+moves_so_far+1, [(succ), route_so_far + " " + move,moves_so_far+1] ))
+            i += 1
     return False
 
 # test cases
@@ -52,6 +63,9 @@ start_state = []
 with open(sys.argv[1], 'r') as file:
     for line in file:
         start_state += [ int(i) for i in line.split() ]
+        print start_state
+
+goal_state = sorted(start_state)
 
 if len(start_state) != 16:
     print "Error: couldn't parse start state file"
