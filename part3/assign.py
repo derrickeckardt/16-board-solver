b3d340c45f8e780cc7623d21b67aa46abbf7ff90
derrick eckardt <derrick.eckardt@gmail.com>
2018-10-01 04:25:27 +0000

    Part3 walkthrough

--- part3/assign.py
+++ part3/assign.py
@@ -1,2 +1,22 @@
-#!/bin/python
-# put your group assignment problem here!
+#!/usr/bin/env python
+# Indiana University Fall 2018 CSCI-B551 Elements of AI
+# Assignment 1, Part 3 - Group Assignments
+#
+# As directed in the assignment, to run this program type the following at the command line:
+# ./assign.py [input-file] [k] [m] [n]
+# 
+# Where:
+# [input-file] is the name of the text file that has everyone's preferences
+# [k] is the minutes it takes to grade
+# [m] is the minutes it takes to meet with the professor for everyone assigned
+#     with someone they did not want to be assigned to
+# [n] is the minutes it takes to respond to email  for every person not assigned
+#     to their goup
+#
+# In general, think of this as a search problem.
+# 
+# Initial State: A blank list of groups
+# Goal State: 
+
+
+print "Oy, vey."
