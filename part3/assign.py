ee2a25df9c36674ad8ef4f1d70825acf5fe8c222
derrick eckardt <derrick.eckardt@gmail.com>
2018-10-05 01:28:23 +0000

    readmeing

--- part3/assign.py
+++ part3/assign.py
@@ -1,36 +1,18 @@
 #!/usr/bin/env python
 # Indiana University Fall 2018 CSCI-B551 Elements of AI
 # Assignment 1, Part 3 - Group Assignments
+##
+###############################################################################
+###############################################################################
 #
-# As directed in the assignment, to run this program type the following at the command line:
-# ./assign.py [input-file] [k] [m] [n]
-# 
-# Where:
-# [input-file] is the name of the text file that has everyone's preferences
-# [k] is the minutes it takes to grade
-# [m] is the minutes it takes to meet with the professor for everyone assigned
-#     with someone they did not want to be assigned to
-# [n] is the minutes it takes to respond to email  for every person not assigned
-#     to their goup
+# A full discussion and details can be found in the Readme file for Part 3, 
+# which is located at:
+# https://github.iu.edu/cs-b551-fa2018/derrick-a1/tree/master/part3
 #
-# In general, think of this as a search problem.
-# 
-# Initial State: Each individual is there own group
-# 
-# Goal State: Students organized into groups that minimize faculty time.
+###############################################################################
+###############################################################################
+
 #
-# Successor: Take one group and combine it into the others. Score it, and then place
-# into priority queue
-#
-# Heurestic Function: Summing the total amount of time that it would take.  What is interesting is
-# that we are looking for a minimum.
-#
-# Cost function: Everytime we merge groups, it will change the cost to the faculty
-#
-# Assumption for large classes ,the amount of version of groups is very high, so will start with our initial
-# groups, and then as we prune, ignore groupings that as we combine them are worse than 
-# our best to date, which we will traffic.
-# 
 
 # Import library
 import pandas as pd
@@ -44,11 +26,11 @@ m = int(sys.argv[3])
 n = int(sys.argv[4])
 
 # Optional max consideration for new stages
-if len(sys.argv) == 6:
+if len(sys.argv) > 5:
     max_buff = 1+(float(sys.argv[5])/100)
 else:
-    # not capping it any useful way
-    max_buff = 10.0
+    # not capping it byanything useful way
+    max_buff = 100000.0
     
 # Define some functions
 # Score the group
@@ -83,9 +65,6 @@ def score_all(groups, k,m,n):
     return all_scores + k_size
 
 def successors(groups):
-    #copy current group
-    # will make one change.
-    # we will take each
     successor_set = []
     groups_iter = groups[0] * 1
     for group in groups_iter:
@@ -110,7 +89,7 @@ def solve(initial_groups):
     fringe = [initial_groups * 1]
     # print "Welcome to the GroupAssign 3000"
     # print "Sorting "+str(len(initial_groups[0]))+" students into groups.  Optimizing for time."
-    # i = 1
+    i = 1
     while len(fringe) > 0:
         # Creates a priority queue
         fringe = sorted(fringe,key=itemgetter(1))
@@ -118,11 +97,11 @@ def solve(initial_groups):
             if score < best_score:
                 best_groups = groups *1
                 best_score = score
-                #worst_acceptable_score = float(best_score)*max_buff
-            #if score < worst_acceptable_score:
-            fringe.append([groups,score])
+                worst_acceptable_score = float(best_score)*max_buff
+            if score < worst_acceptable_score:
+                fringe.append([groups,score])
             # Add a differentiator later to throw out bad values
-    #         i += 1
+            i += 1
     #         if i%1000==0:
     #             print i," states evaluated so far"
     #             print "Best Score: ", best_score
@@ -138,39 +117,6 @@ def print_groups(groups):
         line=line.strip()
         print line
     return None
-
-# Given the scoring factors, there are somethings I can do to help myself, by
-# simplifying the initial state.  Students have already done some work to make
-# life easier on me
-#
-# future Performance enhancements - look for students that have self-selected themselves
-# 
-# First: Look for groups of 3 that self-selected themselves together.  ie, person A said 
-# they wanted B & C; B said they wanted A & c; C said they wanted A & B
-# 
-# Second: Look for a person A and B that self-selected themselves together.  They 
-# will be placed into a group, with a slot available for a third person.  Later 
-# on, we may have to add a third person.  If they didn;t ask for a third person, 
-# if they complain about group size, it's only a 1 minute penalty per person for
-# an otherwise happy group.
-#
-# These first two steps optimize for groups that were already formed and want to
-# work together.  I'm going to assume this is optimal.  Now, there probably some chance
-# this could actually be suboptimal, such as if there is a student in very
-# high demand, such that a number greater than 2 persons wanted to work with them.
-# There might be some optimally lost by actually breaking up the group.  As the course
-# instructor, I'm willing to accept that if people self-organize, I'm willing take
-# a short-term penalty for long-term student happiness.
-# 
-# These assumptions allow us to also reduce the number of future states to explore
-# which makes our life a lot easier.
-
-# Once completed, I will test all of these assumptions out by not letting them by
-# used, and having the algorithm try to optimize.
-#
-# Test case ex-assign50 was provide by classmate Jonathan Branam via:
-# https://piazza.com/class/jl1erlsbz1n6ax?cid=259 which linked to:
-# https://gist.github.com/jonathanbranam/c2b121237d1954c574b0a2bbc075e662
 
 # Step Zero - Read in the datafile as a list of list
 c_l = pd.read_csv(input_file, delimiter=" ",header=None,names=['student','pref','friends','enemies'])
---
+++ part3/readme.md
@@ -0,0 +1,47 @@
+## Part 3: Group Assignments
+
+Completed by Derrick Eckardt on October 2, 2018.  Please direct any questions to [derrick@iu.edu](mailto:derrick@iu.edu)
+
+The assignment prompt can be found at [Assignment 1 Prompt](https://github.iu.edu/cs-b551-fa2018/derrick-a1/blob/master/a1-v2.pdf).  This readme file provides the required elements and my discussion of the process and the findings.
+
+## Getting Started
+
+As directed in the assignment, to run this program type the following at the command line:
+
+    ./assign.py [input-file] [k] [m] [n]
+
+For a more details on the set-up, please see the [Assignment 1 Prompt](https://github.iu.edu/cs-b551-fa2018/derrick-a1/blob/master/a1-v2.pdf)
+
+## Summary of Problem
+
+In general, think of this as a search problem.
+
+**Initial State:** Each individual is there own group to start.
+
+**Goal State:** Students organized into groups that minimize faculty time for grading and dealing with unhappy students.
+
+**State Space:** Every possibile combination of groups.  For 50 students, it was on the order of 50!/13!.  Huge! Not actually searchable.
+
+**Successor Function:** Take one group and combine it into the others. Score , and then place into priority queue
+
+**Heurestic Function:** Summing the total amount of time that it would take.  What is interesting, is that I know exactly what that value is.  We are looking for a minimum, and do not know exactly when we will have achieved an acceptable level.
+
+**Cost Function:** Everytime I merge groups, it will change the cost of the time to the faculty.  There is no cost to actually make the changes; however, some changes can reduce the time, and others can increase the time.
+
+## Discussion of Approach
+
+In this, I took a look a locla search approach, where I continuously looked for the best scenario.  In order to get the optimal answer, you would have to search the entire state space.  In that case, one has to determine what is acceptable.
+
+For example, When I used the "ex-assign50.txt" dataset, which was provided by a classmate [Jonathan Branam via Piazza](https://piazza.com/class/jl1erlsbz1n6ax?cid=259) for testing, with an input line of k = 10, m = 10, and n = 2, my initial state had a value of 666 minutes.  Keeping all states, after evaluating 13000 states, it had found a state with a value 284 minutes, and continued to search.  Then, I added pruning, to have it throw away any state that was worse than the current best score.  I found that by pruning, the code speed up significantly (by as much as a factor of 10! on smaller classes) and the results were similar.  For the "ex-assign50.txt" dataset, the new minimum was 289 after 13000 states.  After 400,000+ states, it was still the same 289.  While this is a local minimum, it's pretty damn close.  A trade-off I would be willing to make for time and reduced computing resources.
+
+To enable pruning, I enabled the code to take an additional argument and works by entering the following at the command prompt:
+
+    ./assign.py [input-file] [k] [m] [n] [max-buff]
+
+Where [max-buff] is percent, as a two digit number of how much worse than the best will the program accept keeping.  For example typing a value of 20 for max-buff will prune any state where the state is more than 20% worse than the ideal state.
+
+## Opportunities for Improvement
+
+**Refactor, Refactor, Refactor:** This code runs really slow.  For classes larger than 50, it might be really slow.  Just depends on how fast your comptuer is, and home much time you're willing to wait for an optimized class. There are probably a million ways to refactor the code to improve it.
+
+**Look for Gimmes:** In real life, we would have groups that decided to work together on their own, and their inputs reflect this.  We could change our initial state to combine those that clearily preselected themselves.  This would be a group of 3 where A asked for B and C, B asked for A and C, and C asked for A and B.  This could lead to some suboptimality based on other students desire to work with persons A, B, and C.  This is a tradeoff I would be willing to make since these were students who took the initiative to work together.  Similarily, we could also do this for groups of 2 where they asked to work with each (A puts B, and B puts A).  Of course, they would still be subject to having a third person added, but we get farther down the curve.
